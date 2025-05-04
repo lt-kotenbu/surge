3dcad57d4ab2d727988c2696bc24048dd8852330
LT <LT@kotenbu.uk>
2025-05-04 15:42:32 +0800

    Update jego.js

--- jego.js
+++ jego.js
@@ -3,6 +3,7 @@
 const $ = new Env('无忧行');
 const token = $argument;
 console.log(token);
+console.log($argument);
 $.secretKey = 'online_jego_h5';
 $.secretVal = '93EFE107DDE6DE51';
 const currentTimestamp = Date.now()*1000+765; // 使用标准毫秒时间戳
